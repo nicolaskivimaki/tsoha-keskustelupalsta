c551eb85809b111ac1769295fbe0436e8c842f3e
Nicolas Kivimäki <kivimakinicolas@gmail.com>
2022-09-25 20:24:37 +0300

    mm

--- routes.py
+++ routes.py
@@ -1,9 +1,7 @@
 from flask import Flask
+from app import app
 from flask import redirect, render_template, request, session
 from os import getenv
-
-app = Flask(__name__)
-app.secret_key = getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
