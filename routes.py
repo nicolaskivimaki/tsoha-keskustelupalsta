692cde1808eaffa78c6af9045019b7d6dd8f746a
Nicolas Kivimäki <kivimakinicolas@gmail.com>
2022-10-09 11:55:51 +0300

    Login, rekisteröityminen, layout

--- routes.py
+++ routes.py
@@ -1,25 +1,11 @@
-from flask import Flask
-from flask import redirect, render_template, request, session
-from flask_sqlalchemy import SQLAlchemy
 from app import app
-from db import db
+from flask import render_template, request, redirect
+import messages, users
 
 @app.route("/")
 def index():
-    result = db.session.execute("SELECT content FROM messages")
-    messages = result.fetchall()
-    return render_template("index.html", count=len(messages), messages=messages) 
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "GET":
-        return render_template("login.html")
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        # session["username"] = username
-        return redirect("/new")
-    return render_template("login.html")
+    list = messages.get_list()
+    return render_template("index.html", count=len(list), messages=list)
 
 @app.route("/new")
 def new():
@@ -28,7 +14,39 @@ def new():
 @app.route("/send", methods=["POST"])
 def send():
     content = request.form["content"]
-    sql = "INSERT INTO messages (content) VALUES (:content)"
-    db.session.execute(sql, {"content":content})
-    db.session.commit()
+    if messages.send(content):
+        return redirect("/")
+    else:
+        return render_template("error.html", message="Viestin lähetys ei onnistunut")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Väärä tunnus tai salasana")
+
+@app.route("/logout")
+def logout():
+    users.logout()
     return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Salasanat eroavat")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Rekisteröinti ei onnistunut")
